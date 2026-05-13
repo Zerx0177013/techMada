5a5c25eeac79a5e8bf9a25e6619d44158d33f981
Rohan <andriamihaja2007rohan@gmail.com>
2026-05-13 11:54:03 +0300

    route pour annulation de demande de conge

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,6 +14,7 @@ $routes->get('employe', 'EmployesController::dashboard', ['filter' => 'auth']);
 $routes->get('employe/create', 'EmployesController::create', ['filter' => 'auth']);
 $routes->post('employe/store', 'EmployesController::store', ['filter' => 'auth']);
 $routes->get('employe/conges', 'EmployesController::conges', ['filter' => 'auth']);
+$routes->post('employe/conges/cancel/(:num)', 'EmployesController::cancelConge/$1', ['filter' => 'auth']);
 $routes->get('rh', 'Home::rhIndex', ['filter' => ['auth', 'role:rh,admin']]);
 $routes->get('admin', 'Home::adminDashboard', ['filter' => ['auth', 'role:admin']]);
 $routes->get('admin/employes', 'Home::adminEmployes', ['filter' => ['auth', 'role:admin']]);
